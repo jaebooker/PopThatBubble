676cc0ab7619f6a75c751c1e71232cabb4f996f8
jaebooker <jaesonbooker@gmail.com>
2018-09-29 00:31:25 -0700

    cleared out some old code, added speed of bubbles getting faster as time goes along

--- BoutThatBubble/GameScene.swift
+++ BoutThatBubble/GameScene.swift
@@ -14,6 +14,8 @@ class GameScene: SKScene {
     private var label : SKLabelNode?
     private var spinnyNode : SKShapeNode?
     var score: Int = 0
+    var bubbleSpeed: Double = 10.0
+    var bubbleCount: Int = 0
     func createBubble() {
         let bubble: Bubble = Bubble()
         bubble.name = "bubble"
@@ -22,67 +24,36 @@ class GameScene: SKScene {
         bubble.position.x = 425
         bubble.position.y = CGFloat.random(in: Range<CGFloat>(uncheckedBounds: (lower: -627.0, upper: 627.0)))
         addChild(bubble)
+        bubbleCount += 1
         moveThatBubble(bubble: bubble)
     }
     func moveThatBubble(bubble: Bubble) {
         let moveUp = SKAction.moveBy(x: -1000,
                                      y: 0,
-                                     duration: 5.0)
+                                     duration: bubbleSpeed)
         let removeNode = SKAction.removeFromParent()
         let sequence = SKAction.sequence([moveUp, removeNode])
         bubble.run(sequence)
     }
     override func didMove(to view: SKView) {
         // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
+        self.label = self.childNode(withName: "scoreCard") as? SKLabelNode
         if let label = self.label {
             label.alpha = 0.0
             label.run(SKAction.fadeIn(withDuration: 2.0))
             label.text = "\(score)"
         }
-        
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
-//        let moveAction = SKAction.moveBy(x: 10, y: -15, duration: 0.8)
-//        let player = SKSpriteNode(imageNamed: "bibble.jpg")
-//        let moveFiveTimes = SKAction.repeat(moveAction, count: 5)
-//        player.run(moveFiveTimes)
         createBubble()
     }
     
     
     func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
     }
     
     func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
     }
     
     func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -92,12 +63,11 @@ class GameScene: SKScene {
         if atPoint(location).name == "bubble" {
             atPoint(location).removeFromParent()
             score += 1
+            bubbleSpeed -= 0.5
             label!.text = "\(score)"
             createBubble()
             createBubble()
-        }
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
+            bubbleCount -= 1
         }
         for t in touches { self.touchDown(atPoint: t.location(in: self))}
     }
